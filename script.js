4b02459adc3cba02c1645acfc6fa30de21b70f8e
Tom Reekmans <tomreekmans@gmail.com>
2022-07-20 10:16:37 +0700

    distinct array for the messages random number

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-console.log("This is a message to you Rudy!");
-console.log("Check version control functioning");
+const messages = {
+  chant: ["Ohm Mani Padme Hom", "Abracadabra", "In a gada da vida"],
+  focus: [
+    "Clear Negative Energy",
+    "Bannish unhealthy influences",
+    "Embrace your power",
+  ],
+  avoid: ["People with blue shirts", "Pommeranian dogs", "Food with beetroot"],
+};
+
+const mixedMessages = [
+  Math.floor(Math.random() * 3),
+  Math.floor(Math.random() * 3),
+  Math.floor(Math.random() * 3),
+];
+let message = `Your protective chant for this week is: ${
+  messages.chant[mixedMessages[0]]
+} \n
+Your focus should be: ${messages.focus[mixedMessages[1]]} \n
+And you should avoid: ${messages.avoid[mixedMessages[2]]}`;
+console.log(message);
