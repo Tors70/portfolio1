3abd1a24b656f19aeef80024edfee436f60b4ff9
Tom Reekmans <tomreekmans@gmail.com>
2022-07-20 11:16:24 +0700

    This is solution from Codecademy

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function generateRnd(num) {
+  let rnd = Math.floor(Math.random() * num);
+  return rnd;
+}
 const messages = {
   chant: ["Ohm Mani Padme Hom", "Abracadabra", "In a gada da vida"],
   focus: [
@@ -8,9 +12,28 @@ const messages = {
   avoid: ["People with blue shirts", "Pommeranian dogs", "Food with beetroot"],
 };
 
-let message = `Your protective chant for this week is: ${
-  messages.chant[Math.floor(Math.random() * 3)]
-} \n
-Your focus should be: ${messages.focus[Math.floor(Math.random() * 3)]} \n
-And you should avoid: ${messages.avoid[Math.floor(Math.random() * 3)]}`;
-console.log(message);
+let finalMessage = [];
+
+for (let prop in messages) {
+  let num = generateRnd(messages[prop].length);
+  switch (prop) {
+    case "chant":
+      finalMessage.push(
+        `Your protective chant for this week is: ${messages[prop][num]}`
+      );
+      break;
+    case "focus":
+      finalMessage.push(`Your focus should be: ${messages[prop][num]}`);
+      break;
+    case "avoid":
+      finalMessage.push(`And you should avoid: ${messages[prop][num]}`);
+      break;
+    case "default":
+      finalMessage.push(`There is not enough info!`);
+  }
+}
+
+function generateMsg() {
+  console.log(finalMessage.join("\n"));
+}
+generateMsg();
